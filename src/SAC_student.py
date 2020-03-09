c3659dba182f1fc8af770aae25a09e223c5b382a
Woodrow Wang <woody@DN0a1f6c2a.SUNet>
2020-03-08 17:54:01 -0700

    Change reward and state, fix visualizations

--- src/SAC_student.py
+++ src/SAC_student.py
@@ -24,22 +24,29 @@ def main():
         ),
         validation_path=DATA_PATH,
         max_queries=config.MAX_QUERIES)
-    agent_model = SAC(MlpPolicy, env, random_exploration=config.EPSILON_EXPLORATION, verbose=1)
+    agent_model = SAC(MlpPolicy, env, train_freq=1, batch_size=1, learning_rate=3e-4, learning_starts=0, buffer_size=5, random_exploration=config.EPSILON_EXPLORATION, gamma=config.GAMMA, verbose=1)
     agent_model.learn(total_timesteps=config.MAX_QUERIES * config.NUM_TRAIN_EPISODES)
+
+    # TODO: REMOVE This shouldn't be useful
+    agent_model.save('test_SAC')
+    del agent_model # remove to demonstrate saving and loading
+    agent_model = SAC.load('test_SAC')
 
     obs = env.reset()
 
-    reward = float('-inf')
+    total_reward = float('-inf')
     prog = tqdm(
         range(config.MAX_QUERIES),
-        postfix={'Reward': reward}
+        postfix={'Reward': total_reward}
     )
 
     actions = [] # For visualization
+    total_reward = 0.0
     for i in prog:
         action, _states = agent_model.predict(obs)
         obs, reward, done, info = env.step(action)
-        prog.set_postfix({'Reward': reward})
+        total_reward += reward
+        prog.set_postfix({'Reward': total_reward})
         actions.append(np.asscalar(action)) 
     plt.hist(actions, bins=config.NUM_BINS, range=(-5, 5), density=True)
     plt.savefig('./visualizations/histograms/SAC')
@@ -50,6 +57,7 @@ def main():
     outputs = env.student_model(inputs.reshape(-1, 1))
     plt.scatter(inputs, outputs, s=0.1, label='SAC')
     plt.title("SAC Student's Approximation")
+    plt.ylim((-60, 100))
     plt.savefig('./visualizations/functions/SAC')
     plt.clf()
 
